4f958e1e2c18c2537835d639f22e8724fb0d3637
Lokesh <92700236+XavierLokison@users.noreply.github.com>
2025-05-06 19:10:39 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,8 @@ set -e
 
 # Pull the Docker image from Docker Hub
 echo "dckr_pat_oy3T4-Q4zhSP9EbV0zw0siKQ2vo" | docker login -u xavierlokison --password-stdin docker.io
+
+docker ps -q | xargs -r docker stop && docker ps -aq | xargs -r docker rm
 docker pull xavierlokison/simple-flask-app:latest
 
 # Run the Docker image as a container
