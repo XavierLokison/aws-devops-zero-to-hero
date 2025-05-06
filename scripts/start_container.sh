e38564c5c2716322ca975711569eb31d59d70b3d
Lokesh <92700236+XavierLokison@users.noreply.github.com>
2025-05-06 15:12:03 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull xavierlokison/simple-flask-app
+docker pull xavierlokison/simple-flask-app:v1
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 xavierlokison/simple-python-flask-app
+docker run -d -p 5000:5000 xavierlokison/simple-python-flask-app:v1
